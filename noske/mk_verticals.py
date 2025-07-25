37f1c92f6ea75589cd4678e5fa85935b78aa94f5
Daniel Elsner <daniel.elsner@oeaw.ac.at>
2025-07-25 09:28:40 +0200

    updating attr names

--- noske/mk_verticals.py
+++ noske/mk_verticals.py
@@ -185,7 +185,7 @@ def write_to_tsv(output_file: str, data_text: list, data_attributes: list) -> No
     with open(output_file, "a", encoding="utf-8") as f:
         doc_id = os.path.basename(output_file).replace(".tsv", "")
         title = doc_id.replace("_", " ")
-        f.write(f'<doc id="{doc_id}" title="{title}" attrs="word lemma w l pos id plc plcType prs prsType idPb idN">\n')
+        f.write(f'<doc id="{doc_id}" title="{title}" attrs="word lemma w l pos id placeName placeType persName persType idPb idN">\n')
         for idx, text in enumerate(data_text) if data_text else []:
             f.write(text + "\t" + "\t".join(data_attributes[idx]) + "\n")
         f.write("</doc>\n")
